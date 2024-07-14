cbf59a1a3a65bdbf6523bdbdba065460c5de5897
Ryan Earl <ryanleoearl@gmail.com>
2024-07-14 13:24:07 -0700

    Added api gateway and updated lambda to properly receive database queries and return responses

--- functions/example.py
+++ functions/example.py
@@ -1,5 +1,6 @@
 import boto3
 import random
+import json
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb') 
@@ -7,32 +8,39 @@ table = dynamodb.Table('food-decider-dev-food-options')
 restaurants = []
 
 def handler(event, context):
-    food_type = event.get('food_type', 'ALL')
-    visited = event.get('visited', 'ALL')
+    body = json.loads(event['body'])
+    print(body)
+    food_type = body.get('food_type', 'ALL')
+    visited = body.get('visited', 'ALL')
+
+    if visited.lower() == 'yes':
+        visited = 1
+    elif visited.lower() == 'no':
+        visited = 0
+
+    print (food_type, visited)
 
     query = {}
     index_name = None
 
-    if food_type != 'ALL':
-        query['KeyConditionExpression'] = Key('food_type').eq(food_type)
-        index_name = 'food_type-index'
-
-    if visited != 'ALL':
-        if 'KeyConditionExpression' in query:
-            query['KeyConditionExpression'] = query['KeyConditionExpression'] & Key('visited').eq(visited)
-            index_name = 'visited-index'
-        else:
-            query['KeyConditionExpression'] = Key('visited').eq(visited)
-            index_name = 'visited-index'
-
-    if index_name:
-        query['IndexName'] = index_name
+    if food_type != 'ALL' and visited != 'ALL':
+        query['KeyConditionExpression'] = Key('food_type').eq(food_type) & Key('visited').eq(visited)
+        index_name = 'food_type-visited-index'
+    elif visited != 'ALL':
+        query['KeyConditionExpression'] = Key('visited').eq(visited)
+        index_name = 'visited-index'
 
     if query:
+        query['IndexName'] = index_name
+        print(query)
         response = table.query(**query)
     else:
         response = table.scan()
     items = response['Items']
     random_item = random.choice(items)
-
-    return random_item['name']
+    response = {
+        "statusCode": 200,
+        "body": random_item['name']
+    }
+    print(response)
+    return response
